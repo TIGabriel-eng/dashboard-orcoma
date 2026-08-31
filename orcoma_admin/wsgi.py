cd1b7825daac4808401b5c1a4229285c47c78e39
gabrielanacleto-maker <gabriel.anacleto.cruz@gmail.com>
2026-08-31 13:15:41 -0300

    !

--- orcoma_admin/wsgi.py
+++ orcoma_admin/wsgi.py
@@ -14,3 +14,6 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'orcoma_admin.settings')
 
 application = get_wsgi_application()
+
+from django.core.management import call_command
+call_command('migrate', '--run-syncdb', verbosity=0)
